ad63b28955df1491a417e570eb7e89b787788a95
Gan-Y-H <72074352+Gan-Y-H@users.noreply.github.com>
2022-04-19 17:20:49 -0500

    Add files via upload
    changed enA-->8 to enA-->3, the problem with pin 8 is that it cannot transmit signal.

--- ground_motor.ino
+++ ground_motor.ino
@@ -1,7 +1,7 @@
-#define enA 9
+#define enA 3
 #define in1 6
 #define in2 5
-#define enB 8
+#define enB 9
 #define in3 11
 #define in4 10
 
@@ -79,39 +79,4 @@ void loop() {
   delay(10000);
   forward(100);
   delay(10000);
-  /*
-  //forward
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  //analogWrite(enA, 255);
-  //analogWrite(enB, 255);
-  delay(2000);
-  
-  //stop
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-  delay(1000);
-  
-  //backward
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  //analogWrite(enA, 255);
-  //analogWrite(enB, 255);
-  delay(2000);
-
-  //stop                                                                                             
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-  delay(1000);
-//  
-*/
-
 }
